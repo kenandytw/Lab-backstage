27b6498ddb01132331f1b76c85781c2b957fa9a1
fufukwang <fu@thenewslens.com>
2016-06-04 22:39:19 +0800

    update text

--- resources/views/backstage/order/edit.blade.php
+++ resources/views/backstage/order/edit.blade.php
@@ -72,35 +72,33 @@
                                         <div class="form-group">
                                             <label class="col-sm-3 control-label">訂單狀態</label>
                                             <div class="col-sm-6">
-                                                <input data-parsley-type="number" type="text" class="form-control" required placeholder="Enter only numbers" readonly value="{{ $order->Status }}" />
+                                                <input data-parsley-type="number" type="text" class="form-control" readonly value="{{ $order->Status }}" />
                                             </div>
                                         </div>
                                         @if($order->Pay=='online')
                                         <div class="form-group">
                                             <label class="col-sm-3 control-label">刷卡訊息</label>
                                             <div class="col-sm-6">
-                                                <input data-parsley-type="number" type="text" class="form-control" required placeholder="Enter only numbers" readonly value="{{ $order->Message }}" />
+                                                <input data-parsley-type="number" type="text" class="form-control" readonly value="{{ $order->Message }}" />
                                             </div>
                                         </div>
                                         @endif
                                         <div class="form-group">
                                             <label class="col-sm-3 control-label">註記</label>
                                             <div class="col-sm-6">
-                                                <textarea required class="form-control" name="PS">{{ $order->PS }}</textarea>
+                                                <textarea class="form-control" name="PS">{{ $order->PS }}</textarea>
                                             </div>
                                         </div>
-                                        @if($order->Pay=='onsite')
                                         <div class="form-group">
                                             <div class="col-sm-offset-3 col-sm-6">
                                                 <div class="checkbox checkbox-primary">
-                                                    <input id="checkbox1" type="checkbox" name="Cancel" value="1">
+                                                    <input id="checkbox1" type="checkbox" name="Cancel" value="1"{{ $order->Status=='SUCCESS' ? '' : ' disabled checked' }}>
                                                     <label for="checkbox1">
-                                                        取消訂位
+                                                        取消訂位(訂單取消後為方便統計人數不提供恢復訂單功能,請謹慎使用)
                                                     </label>
                                                 </div>
                                             </div>
                                         </div>
-                                        @endif
                                         <div class="form-group">
                                             <div class="col-sm-offset-3 col-sm-9 m-t-15">
                                                 <button type="submit" class="btn btn-primary waves-effect waves-light">
