eb08de63dae62aba4a79eb3a5ab1384f868a72e8
Randy Hsieh <randy@thenewslens.com>
2016-07-10 00:49:52 +0800

    更新 people 頁面 還有 css

---
+++ resources/views/frontend/people.blade.php
@@ -0,0 +1,37 @@
+@extends('frontend.layout')
+
+
+@section('content')      
+        <!-- first cut -->
+         
+        <div class="section-02 people-section">
+           
+            <div class="people-nav">
+                <ul>
+                    <li class="after-arrow">
+                        <div>1</div>
+                        <span># 無光晚餐</span>
+                    </li>
+                    <li class="after-arrow">
+                        <div>2</div>
+                        <span style="margin-top:-10px;">
+                            Instagram 上傳照片<br>
+                            <span class="hidden-xs">&nbsp;&nbsp;</span>同步分享至Facebook
+                        </span>
+                    </li>
+                    <li>
+                        <div>3</div>
+                        <span>
+                            分享屬於自己的無光體驗
+                        </span>
+                    </li>
+                </ul>
+            </div>
+           
+           <div style="min-height:460px;">
+            
+           </div>
+            
+            
+        </div>     
+@endsection
