068891f1f877d2499e94df946f51765b181f0247
fufukwang <fu@thenewslens.com>
2016-08-05 00:40:48 +0800

    upload html file

---
+++ resources/views/frontend/press.blade.php
@@ -0,0 +1,246 @@
+@extends('frontend.layout')
+
+
+@section('content')      
+        <!-- first cut -->
+
+        <div class="section-02 press-section">
+            <div class="main-container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <a target="_blank" href="http://travel.ettoday.net/article/729597.htm">
+                           <div class="press-box">
+                               <div class="press-description">
+                                   <h1>
+                                       ETtoday
+                                   </h1>
+                                   <p>
+                                       全黑中吃飯現在台灣也有了！
+                                   </p>
+                               </div>
+
+                               <img src="/images/press/1.ettoday.png" alt="ettoday">
+                           </div>
+                       </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a target="_blank" href="http://www.gq.com.tw/life/food/content-28481.html">
+                            <div class="press-box">
+                                <div class="press-description">
+                                    <h1>
+                                        GQ Taiwan
+                                    </h1>
+                                    <p>
+                                        台北近期討論度最高的餐廳
+                                    </p>
+                                </div>
+
+                                <img src="/images/press/2.gq.png" alt="gq">
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a target="_blank" href="http://www.marieclaire.com.tw/lifestyle/taste/24991">
+                            <div class="press-box">
+                                <div class="press-description">
+                                    <h1>
+                                        美麗佳人 Marie Claire
+                                    </h1>
+                                    <p>
+                                        黑暗中挑戰你的味蕾與未知
+                                    </p>
+                                </div>
+
+                                <img src="/images/press/3.marieclaire.png" alt="marieclaire">
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a target="_blank" href="http://www.vogue.com.tw/feature/foods/content-28068.html">
+                            <div class="press-box">
+                                <div class="press-description">
+                                    <h1>
+                                        VOGUE
+                                    </h1>
+                                    <p>
+                                        來約一場無光晚餐，保證你對吃飯完全改觀
+                                    </p>
+                                </div>
+
+                                <img src="/images/press/4.vogue.png" alt="vogue">
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a target="_blank" href="http://www.elle.com.tw/lifestyle/gourment/dining-dark-project-taipei/">
+                            <div class="press-box">
+                                <div class="press-description">
+                                    <h1>
+                                        ELLE
+                                    </h1>
+                                    <p>
+                                        放下萬惡的手機，來一場未知的探索旅程
+                                    </p>
+                                </div>
+
+                                <img src="/images/press/5.elle.png" alt="elle">
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a target="_blank" href="contact.html">
+                            <div class="press-box">
+                          
+
+                                <img src="/images/press/6.contact.png" alt="contact">
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a target="_blank" href="http://www.mydesy.com/surprise-lab-did">
+                            <div class="press-box">
+                                <div class="press-description">
+                                    <h1>
+                                        ㄇㄞˋ點子
+                                    </h1>
+                                    <p>
+                                        如果價錢可以換到高級料理，那無光晚餐可以換到認真品味
+                                    </p>
+                                </div>
+
+                                <img src="/images/press/7.mydesy.png" alt="ㄇㄞˋ點子">
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a target="_blank" href="http://www.adaymag.com/2016/07/14/dining-in-the-dark-taipei.html">
+                            <div class="press-box">
+                                <div class="press-description">
+                                    <h1>
+                                        A Day Magazine
+                                    </h1>
+                                    <p>
+                                        走入真愛每一天的劇情裡
+                                    </p>
+                                </div>
+
+                                <img src="/images/press/8.adaymag.png" alt="A Day Magazine">
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a target="_blank" href="https://www.juksy.com/archives/55695">
+                            <div class="press-box">
+                                <div class="press-description">
+                                    <h1>
+                                        Juksy
+                                    </h1>
+                                    <p>
+                                        誰說約會一定要浪漫餐廳？無光晚餐讓你想像無極限
+                                    </p>
+                                </div>
+
+                                <img src="/images/press/9.juksy.png" alt="Juksy">
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a target="_blank" href="http://www.everydayobject.us/2016/07/07/dining-in-the-dark/">
+                            <div class="press-box">
+                                <div class="press-description">
+                                    <h1>
+                                        Everyday Object
+                                    </h1>
+                                    <p>
+                                        刺激我們重新審視「感官」賦予的記憶
+                                    </p>
+                                </div>
+
+                                <img src="/images/press/10.everydayobject.png" alt="Everyday Object">
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a target="_blank" href="http://nommagazine.com/無光晚餐，你賞不賞光？/">
+                            <div class="press-box">
+                                <div class="press-description">
+                                    <h1>
+                                        NOM Magazine
+                                    </h1>
+                                    <p>
+                                        充滿玩心的無光晚餐之旅，你賞不賞光？
+                                    </p>
+                                </div>
+
+                                <img src="/images/press/11.nom.png" alt="NOM Magazine">
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a target="_blank" href="http://food.cna.com.tw/eatandplay/20160712S015.aspx">
+                            <div class="press-box">
+                                <div class="press-description">
+                                    <h1>
+                                        中央社
+                                    </h1>
+                                    <p>
+                                        給大家最極致的暗黑用餐體驗
+                                    </p>
+                                </div>
+
+                                <img src="/images/press/12.cnn.png" alt="NOM Magazine">
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a target="_blank" href="http://flipermag.com/2016/07/07/dining-dark">
+                            <div class="press-box">
+                                <div class="press-description">
+                                    <h1>
+                                        FLiPER
+                                    </h1>
+                                    <p>
+                                        極吸引人的無光晚餐
+                                    </p>
+                                </div>
+
+                                <img src="/images/press/13.fliper.png" alt="FLiPER">
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a target="_blank" href="http://www.walkerland.com.tw/subject/view/118611">
+                            <div class="press-box">
+                                <div class="press-description">
+                                    <h1>
+                                        WalkerLand 窩客島
+                                    </h1>
+                                    <p>
+                                        就算摸黑也要吃
+                                    </p>
+                                </div>
+
+                                <img src="/images/press/14.walkerland.png" alt="WalkerLand 窩客島">
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a target="_blank" href="http://news.tvbs.com.tw/travel/661064">
+                            <div class="press-box">
+                                <div class="press-description">
+                                    <h1>
+                                        TVBS
+                                    </h1>
+                                    <p>
+                                        給消費者不同的用餐經驗
+                                    </p>
+                                </div>
+
+                                <img src="/images/press/15.tvbs.png" alt="TVBS">
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+@endsection
