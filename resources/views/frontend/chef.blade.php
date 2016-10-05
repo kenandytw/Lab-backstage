0a45c4c37d8df3ec2f436efa3671c591e26aec5b
Randy Hsieh <randy@thenewslens.com>
2016-10-05 19:01:19 +0800

    刪除多餘的空白

--- resources/views/frontend/chef.blade.php
+++ resources/views/frontend/chef.blade.php
@@ -9,8 +9,8 @@
                     {!! trans('front.chef.standard') !!}
                 </div>
             </div>
-            <div class="main-container">
-                <!--<div class="main-box hidden-xs">
+            <!--div class="main-container">
+                <<div class="main-box hidden-xs">
                     <p>{!! trans('front.chef.section') !!}</p>
                     <h2 class="mg-top-45" style="color: #a67c52;">
                         {{ trans('front.chef.name') }}
@@ -27,8 +27,8 @@
                         </h2>
                     </div>
                     <div class="chef-info">{!! trans('front.chef.exp') !!}</div>
-                </div>-->
-            </div>
+                </div>>
+            </div-->
             <!--<div class="main-container main-even-container">
                 <div class="main-box">
                     <img class="hidden-xs" src="/images/cut3_about/menu.png" alt="">
